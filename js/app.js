57fd31ba6739062eb32819111b92b4e5a02f0465
João Sarmento <joao.sarmento@jumia.com>
2024-01-22 18:31:30 +0000

    pc game

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const GRID_SIZE = GRID_WIDTH * GRID_HEIGHT;
     const INITIAL_POSITION = 4;
     const TIMER = 1000;
+    let level = 0;
+    const ROTATIONS = 4;
 
     const colors = [
         'url(images/blue_block.png)',
@@ -15,11 +17,17 @@ document.addEventListener('DOMContentLoaded', () => {
     ];
 
     const startButton = document.querySelector('.button');
+    const scoreDisplay = document.querySelector('.score-display');
+    const linesDisplay = document.querySelector('.lines-score');
+    const levelDisplay = document.querySelector('.level-score');
     
     let timerId;
+    let isGameOver = false;
+    let score = 0;
+    let lines = 0;
 
     let currentRotation = 0;
-    let nextRandomPiece = 0;
+    let nextRandomPiece = null;
     let displayIndex = 0;
     const displayWidth = 4;
 
@@ -99,13 +107,13 @@ document.addEventListener('DOMContentLoaded', () => {
     function control(e) {
         switch(e.keyCode) {
             case 37:
-                //moveLeft();
+                moveLeft();
                 break;
             case 38:
-                //rotate();
+                rotate();
                 break;
             case 39:
-                //moveRight();
+                moveRight();
                 break;
             case 40:
                 moveDown();
@@ -114,8 +122,6 @@ document.addEventListener('DOMContentLoaded', () => {
                 break;
         }
     }
-
-    document.addEventListener('keydown', control);
 
     let randomPiece = Math.floor(Math.random() * theTetrominoes.length);
     let currentPiece = theTetrominoes[randomPiece][currentRotation];
@@ -139,6 +145,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function freeze() {
         if (currentPiece.some( index => squares[currentPosition + index + GRID_WIDTH].classList.contains('block3') ||
         squares[currentPosition + index + GRID_WIDTH].classList.contains('block2') )) {
+            currentRotation = 0;
             currentPiece.forEach(index => squares[index + currentPosition].classList.add('block2'));
             randomPiece = nextRandomPiece;
             nextRandomPiece = Math.floor(Math.random() * theTetrominoes.length);
@@ -146,6 +153,8 @@ document.addEventListener('DOMContentLoaded', () => {
             currentPosition = INITIAL_POSITION;
             draw();
             displayPreview();
+            clearLine();
+            gameOver();
         }
     }
 
@@ -156,16 +165,86 @@ document.addEventListener('DOMContentLoaded', () => {
         freeze();
     }
 
+    function moveRight() {
+        const isAtRightEdge = currentPiece.some(index => (currentPosition + index) % GRID_WIDTH === GRID_WIDTH - 1);
+        const hasBlock = currentPiece.some(index => squares[currentPosition + index + 1].classList.contains('block2'));
+        if (!isAtRightEdge && !hasBlock) {
+            undraw();
+            currentPosition += 1;
+            draw();
+        }
+    }
 
+    function moveLeft() {
+        const isAtLeftEdge = currentPiece.some(index => (currentPosition + index) % GRID_WIDTH === 0);
+        const hasBlock = currentPiece.some(index => squares[currentPosition + index - 1].classList.contains('block2'));
+        if (!isAtLeftEdge && !hasBlock) {
+            undraw();
+            currentPosition -= 1;
+            draw();
+        }
+    }
+
+    function isAtRight() {
+        return currentPiece.some(index=> (currentPosition + index + 1) % GRID_WIDTH === 0)  
+      }
+      
+      function isAtLeft() {
+        return currentPiece.some(index=> (currentPosition + index) % GRID_WIDTH === 0)
+      }
+      
+      function checkRotatedPosition(P){
+        P = P || currentPosition       
+        if ((P+1) % GRID_WIDTH < 4) {  
+          if (isAtRight()){            
+            currentPosition += 1    
+            checkRotatedPosition(P) 
+            }
+        }
+        else if (P % GRID_WIDTH > 5) {
+          if (isAtLeft()){
+            currentPosition -= 1
+          checkRotatedPosition(P)
+          }
+        }
+      }
+
+    function rotate() {
+        undraw();
+        currentRotation++;
+        if (currentRotation === ROTATIONS) {
+            currentRotation = 0;
+        }
+        currentPiece = theTetrominoes[randomPiece][currentRotation];
+        checkRotatedPosition();
+        draw();
+    }
+
+    function gameOver() {
+        if (currentPiece.some(index => squares[currentPosition + index].classList.contains('block2'))) {
+            clearInterval(timerId); 
+            timerId = null;
+            document.removeEventListener('keydown', control);
+            isGameOver = true;
+        }
+    }
 
     startButton.addEventListener('click', () => {
         if (timerId) {
             clearInterval(timerId);
             timerId = null;
+            document.removeEventListener('keydown', control);
         } else {
+            if (isGameOver) {
+                resetScreen();
+                isGameOver = false;
+            }
+            document.addEventListener('keydown', control);
             draw();
-            timerId = setInterval(moveDown, TIMER);
-            nextRandomPiece = Math.floor(Math.random() * theTetrominoes.length);
+            timerId = setInterval(moveDown, TIMER - (level * 10));
+            if(nextRandomPiece === null) {
+                nextRandomPiece = Math.floor(Math.random() * theTetrominoes.length);
+            }
             displayPreview();
         }
     });
@@ -182,6 +261,58 @@ document.addEventListener('DOMContentLoaded', () => {
             displaySquares[displayIndex + index].style.backgroundImage = colors[nextRandomPiece];
         })
 
+    }
+
+    function resetScreen() {
+        for( currentIndex = 0; currentIndex < GRID_SIZE; currentIndex += 1) {
+            score = 0;            
+            lines = 0;
+            level = 0;
+            scoreDisplay.innerHTML = score;
+            linesDisplay.innerHTML = lines;
+            levelDisplay.innerHTML = level + 1;
+            squares[currentIndex].style.backgroundImage = 'none';
+            squares[currentIndex].classList.remove('block2');
+        }
+    }
+
+    function clearLine() {
+
+        for( currentIndex = 0; currentIndex < GRID_SIZE; currentIndex += GRID_WIDTH) {
+            const row = [currentIndex, currentIndex + 1, currentIndex + 2, currentIndex + 3, currentIndex + 4, currentIndex + 5, currentIndex + 6, currentIndex + 7, currentIndex + 8, currentIndex + 9]
+            if (row.every(index => squares[index].classList.contains('block2'))) {
+                score += 10;
+                lines += 1;
+                scoreDisplay.innerHTML = score;
+                linesDisplay.innerHTML = lines;
+
+                row.forEach( index => {
+                    squares[index].style.backgroundImage = 'none';
+                    squares[index].classList.remove('block2')
+                });
+
+                const squaresRemoved = squares.splice(currentIndex, GRID_WIDTH);
+                squares = squaresRemoved.concat(squares);
+                squares.forEach(cell => grid.appendChild(cell));
+
+                levelUp();
+            }
+        }
+
+    }
+
+    function levelUp() {
+        if ( lines < 5) level = 0;
+        else if ( lines >= 5 && lines < 10) level = 1;
+        else if ( lines >= 10 && lines < 15) level = 2;
+        else if ( lines >= 15 && lines < 20) level = 3;
+        else if ( lines >= 20 && lines < 25) level = 4;
+        else if ( lines >= 25 && lines < 30) level = 5;
+        else if (lines > 30) level = 6;
+
+        clearInterval(timerId);
+        timerId = setInterval(moveDown, TIMER - (level * 100));
+        levelDisplay.innerHTML = level + 1;
     }
 
 
